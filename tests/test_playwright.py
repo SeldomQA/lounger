3635781d881aff20daba593cd17ead76d7eea25e
虫师 <defnngj@gmail.com>
2024-07-17 19:03:03 +0800

    test: playwright demo

---
+++ tests/test_playwright.py
@@ -0,0 +1,28 @@
+"""
+playwright demo
+
+run test：
+> pytest --browser=chromium  --headed -vs
+"""
+import re
+import time
+from playwright.sync_api import Page, expect
+
+
+def test_has_title(page: Page):
+    page.goto("https://cn.bing.com")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Bing"))
+
+
+def test_get_started_link(page: Page):
+    page.goto("https://cn.bing.com")
+    input = page.locator("#sb_form_q")
+    input.type("playwright")
+    input.press("Enter")
+    time.sleep(5)
+
+    # Expects page to have a heading with the name of "Installation | Playwright".
+    search_result = page.get_by_role("link", name='Installation | Playwright').first
+    expect(search_result).to_be_visible()
