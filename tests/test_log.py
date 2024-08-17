67ea0a5e6652f4b88ccd976d092c723aed3e4df9
虫师 <defnngj@gmail.com>
2024-08-17 18:49:34 +0800

    test: add test log

---
+++ tests/test_log.py
@@ -0,0 +1,9 @@
+import logging
+
+
+def test_example():
+    logging.debug("This is a debug message")
+    logging.info("This is an info message")
+    logging.warning("This is a warning message")
+    logging.error("This is a error message")
+    assert True
