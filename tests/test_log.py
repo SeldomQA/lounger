5595a82e31704890a07c131e21bde6a322485b59
虫师 <defnngj@gmail.com>
2024-08-25 21:08:20 +0800

    test: update log case.

--- tests/test_log.py
+++ tests/test_log.py
@@ -1,9 +1,26 @@
 import logging
 
+from loguru import logger
 
-def test_example():
+
+def test_sys_log():
+    """sys logging log"""
     logging.debug("This is a debug message")
     logging.info("This is an info message")
     logging.warning("This is a warning message")
     logging.error("This is a error message")
-    assert True
+
+
+def test_loguru_log():
+    """loguru log"""
+    logger.info("loguru 日志")
+    logger.info("loguru 错误")
+
+
+def test_requests_log(get):
+    """
+    get request log (loguru)
+    """
+    payload = {'key1': 'value1', 'key2': 'value2'}
+    s = get(f"https://httpbin.org/get", params=payload)
+    assert s.status_code == 200
