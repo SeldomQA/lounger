6fd773e5b94aeec5b39f84594d42b77ecb82848c
defnngj <defnngj@gmail.com>
2024-08-31 23:30:43 +0800

    test: update test case

--- tests/test_request.py
+++ tests/test_request.py
@@ -4,6 +4,7 @@ requests demo
 run test：
 > pytest -vs --base-url https://httpbin.org test_request.py
 """
+from pytest_req.assertions import expect
 
 
 def test_put_method(put, base_url):
@@ -11,7 +12,7 @@ def test_put_method(put, base_url):
     test put request
     """
     s = put(f'{base_url}/put', data={'key': 'value'})
-    assert s.status_code == 200
+    expect(s).to_be_ok()
 
 
 def test_post_method(post, base_url):
@@ -19,7 +20,7 @@ def test_post_method(post, base_url):
     test post request
     """
     s = post(f'{base_url}/post', data={'key': 'value'})
-    assert s.status_code == 200
+    expect(s).to_be_ok()
 
 
 def test_get_method(get, base_url):
@@ -28,7 +29,7 @@ def test_get_method(get, base_url):
     """
     payload = {'key1': 'value1', 'key2': 'value2'}
     s = get(f"{base_url}/get", params=payload)
-    assert s.status_code == 200
+    expect(s).to_be_ok()
 
 
 def test_delete_method(delete, base_url):
@@ -36,7 +37,7 @@ def test_delete_method(delete, base_url):
     test delete request
     """
     s = delete(f'{base_url}/delete')
-    assert s.status_code == 200
+    expect(s).to_be_ok()
 
 
 def test_patch_method(patch, base_url):
@@ -45,7 +46,7 @@ def test_patch_method(patch, base_url):
     """
     data = {'key': 'value'}
     s = patch(f"{base_url}/patch", data=data)
-    assert s.status_code == 200
+    expect(s).to_be_ok()
 
 
 def test_head_method(head, base_url):
@@ -53,7 +54,7 @@ def test_head_method(head, base_url):
     test head request
     """
     s = head(f"{base_url}/get")
-    assert s.status_code == 200
+    expect(s).to_be_ok()
 
 
 def test_options_method(options, base_url):
@@ -61,4 +62,4 @@ def test_options_method(options, base_url):
     test options request
     """
     s = options(f"{base_url}/get")
-    assert s.status_code == 200
+    expect(s).to_be_ok()
