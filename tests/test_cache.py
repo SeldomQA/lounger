53cb2bba9a5893bdb61bd4807709c86bd5f05c12
defnngj <defnngj@gmail.com>
2024-09-13 00:08:09 +0800

    test: update cache case.

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,25 +1,49 @@
 from lounger.utils import cache
 
-# 清除指定缓存
-cache.clear()
 
-# 获取指定缓存
-token = cache.get("token")
-print(f"token: {token}")
+def setup_function():
+    """
+    测试前执行
+    """
+    cache.set({"key1": "value1", "key2": "value2"})
 
-# 判断为空写入缓存
-if token is None:
+
+def test_get_cache():
+    d = cache.get("key1")
+    assert d == "value1"
+
+
+def test_set_cache():
+    """
+    设置缓存
+    """
     cache.set({"token": "123"})
+    t = cache.get("token")
+    assert t == "123"
 
-# 设置存在的数据(相当于更新)
-cache.set({"token": "456"})
 
-# value复杂格式设置存在的数据
-cache.set({"user": [{"name": "tom", "age": 11}]})
+def test_set_cache_more():
+    """
+    设置更复杂的缓存数据
+    """
+    cache.set({"user": [{"name": "tom", "age": 11}]})
+    m = cache.get("user")
+    assert m == [{"name": "tom", "age": 11}]
 
-# 获取所有缓存
-all_token = cache.get()
-print(f"all: {all_token}")
 
-# 清除指定缓存
-cache.clear("token")
+def test_clear_cache():
+    """
+    清除指定数据
+    """
+    cache.clear("key1")
+    d = cache.get("key1")
+    assert d is None
+
+
+def test_clear_all_cache():
+    """
+    清除所有缓存
+    """
+    cache.clear()
+    d = cache.get()
+    assert d == {}
