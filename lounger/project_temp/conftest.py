57e44bcbc50b3daa48a9a48e33cde4a230f9009b
虫师 <defnngj@gmail.com>
2026-08-15 11:00:56 +0800

    feat: update template

--- lounger/project_temp/conftest.py
+++ lounger/project_temp/conftest.py
@@ -12,3 +12,12 @@ def pytest_xhtml_report_title(report):
 #     webhook_url=_extractor.config("dingtalk_webhook"),
 #     secret=_extractor.config("dingtalk_secret"),
 # )
+
+# Optional template function registration example:
+#
+# from lounger.runtime import register_template_func
+#
+# def random_email():
+#     return "user@example.com"
+#
+# register_template_func("random_email", random_email)
